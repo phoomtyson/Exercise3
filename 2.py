88eb2897630c430cc203da3efdb516f9726eed20
phoomtyson <88759272+phoomtyson@users.noreply.github.com>
2021-10-13 21:06:57 +0700

    Initial commit

---
+++ 2.py
@@ -0,0 +1,33 @@
+print("This is a grade calculator program for each subject")
+p = input("Your score(out of one hundred)")
+p = int(p)
+if p >=80:
+    print ("Your grade is <4.00> <A+>")
+    print ("Excellent")
+elif p <=15:
+    print ("Your grade is <0> <F>")
+    print ("Needs improvement")
+elif p <=25:
+    print ("Your grade is <0> <F>")
+    print ("You good bro?")
+elif p <= 50:
+    print  ("Your grade is <0> <F>")
+    print  ("You failed this class better luck next time")
+elif p <=54
+    print ("Your grade is <1.0> <D>")
+    print ("Unlucky")
+elif p <=59
+    print ("Your grade is <1.5> <D+>")
+    print ("Not good bro")
+elif p <=64
+    print ("Your grade is <2.0> <C>")
+    print ("Could be better")
+elif p <=69
+    print ("Your grade is <2.5> <C+>")
+    print ("You can do better")
+elif p <=74
+    print ("Your grade is <3.0> <B>")
+    print ("Alright that's not bad")
+elif p>79
+    print ("Your grade is <3.5> <B+>")
+    print ("So close to greatness, Great job tho")
---
+++ main.py
@@ -0,0 +1,15 @@
+print("this is a vaccine selector program")
+print("Has the patient received any prior vaccination? (1=yes,3=no")
+n = input("Yes/No")
+n = int(n)
+if n < 2
+    print("this patient receive -1 shot of pfizer vaccine")
+if n > 2
+    print("Has this patient been affected with Sars-cov 2 virus before(1=yes,3=no)")
+o = input("Yes/No")
+o = int(o)
+if o < 2
+    print("This patient receive -1 shot of pfizer vaccine")
+if o > 2
+    print("This patient receives -2 shots of pfizer vaccine")
+
